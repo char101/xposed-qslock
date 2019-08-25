3c002ae78c0aaa275e114a49775524192f818979
Charles <peacech@gmail.com>
2019-08-25 11:32:44 +0700

    Merge pie version

--- app/src/main/java/com/github/char101/qslock/Main.java
+++ app/src/main/java/com/github/char101/qslock/Main.java
@@ -13,24 +13,9 @@ import android.util.Log;
 public class Main implements IXposedHookLoadPackage {
     private static final String TAG = "qslock";
 
-    private static final int DISABLE_EXPAND = 0x00010000;
-    private static final int DISABLE_NOTIFICATION_ICONS = 0x00020000;
-    private static final int DISABLE_NOTIFICATION_ALERTS = 0x00040000;
-    private static final int DISABLE_NOTIFICATION_TICKER = 0x00080000;
-    private static final int DISABLE_SYSTEM_INFO = 0x00100000;
-    private static final int DISABLE_HOME = 0x00200000;
-    private static final int DISABLE_RECENT = 0x01000000;
-    private static final int DISABLE_BACK = 0x00400000;
-    private static final int DISABLE_CLOCK = 0x00800000;
-    private static final int DISABLE_SEARCH = 0x02000000;
-    private static final int DISABLE_NONE = 0x00000000;
-
+    private static final int DISABLE2_NONE = 0;
     private static final int DISABLE2_QUICK_SETTINGS = 1;
-    private static final int DISABLE2_SYSTEM_ICONS = 1 << 1;
     private static final int DISABLE2_NOTIFICATION_SHADE = 1 << 2;
-    private static final int DISABLE2_GLOBAL_ACTIONS = 1 << 3;
-    private static final int DISABLE2_ROTATE_SUGGESTIONS = 1 << 4;
-    private static final int DISABLE2_NONE = 0x00000000;
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.android.systemui")) {
